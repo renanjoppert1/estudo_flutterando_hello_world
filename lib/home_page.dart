557349b4a226b770329011737f11272303e18753
Renan Joppert <renanljp@gmail.com>
2022-03-15 19:50:10 -0300

    Posições absolutas

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -35,15 +35,22 @@ class _HomePageState extends State<HomePage> {
         child: Stack(
           alignment: Alignment.center,
           children: [
-            Container(
-              color: Colors.red,
-              height: 100,
-              width: 100,
-            ),
+            SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.network(
+                  'https://news.microsoft.com/wp-content/uploads/prod/sites/42/2020/09/MS_Penguin-Counting-Story_1900x800-1600x674-1-960x629.jpg',
+                  fit: BoxFit.cover,
+                )),
             Container(
               color: Colors.orange,
               height: 75,
               width: 75,
+            ),
+            Container(
+              color: Colors.blue,
+              height: 35,
+              width: 35,
             ),
           ],
         ),
